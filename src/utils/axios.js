54c7a6114c69cfb0864c9bd96988a34618b6d6ab
Ntish Choudhary <nitishchoudhary15e@gmail.com>
2025-10-17 01:07:51 +0530

    k

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://server-f8bw.onrender.com/api',
+  baseURL: process.env.REACT_APP_API_URL || '/api',
   withCredentials: true, // Essential for cookies
 });
 
